3e57f7e99ad5307f14b5f82ead308b92cbcffc9b
micaeljin <122179001+micaeljin@users.noreply.github.com>
2026-08-11 20:34:34 +0100

    Add files via upload

--- train_gpt2.py
+++ train_gpt2.py
@@ -33,6 +33,44 @@ class CasualSelfAttention(nn.Module):
         return y
 
 
+class MLP(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.c_fc = nn.Linear(config.n_embd, 4*config.n_embd)
+        self.gelu = nn.GELU(approximate='tanh')
+        self.c_proj = nn.Linear(4*config.n_emb, config.n_embd)
+        self.c_proj.NANOGPT_SCALE_INIT = 1
+
+    def forward(self, x):
+        x = self.cfn(x)
+        x = self.gelu(x)
+        x = self.c_proj(x)
+        return x
+
+    
+class Block(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.ln_1 = nn.LayerNorm(config.n_embd)
+        self.attn = CasualSelfAttention(config)
+        self.ln_2 = nn.LayerNorm(config.n_embd)
+        self.mlp = MLP(config)
+
+    def forward(self, x):
+        x = x + self.attn(self.ln_1(x))
+        x = x + self.mlp(self.ln_2(x))
+        return x
+
+
+@dataclass
+class GPTConfig:
+    block_size = 1024
+    vocab_size = 50257
+    n_layer = 12
+    n_head = 12
+    n_embd = 768
 
 
 
